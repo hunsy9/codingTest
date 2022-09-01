6d967162c5bf0789beac7f8914a9abfe4ec8f825
seunghun <juniper0917@gmail.com>
2022-09-01 14:21:34 +0900

    first Prac

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,2 +1,13 @@
-msg="hello world"
-print(msg)
+import io;
+import NumPy;
+
+msg="안녕하세요 저는 유승훈입니다."
+list = []
+list.append(9)
+list.append(10)
+print(list)
+print(len(list))
+list.pop(1)
+print(list)
+print(type(True))
+print(10 & 3)
