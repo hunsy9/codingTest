8e2e31e3db789dcbbbe8ee441bdd0d09c2ca59ff
seunghun <juniper0917@gmail.com>
2022-09-04 21:12:41 +0900

    사칙연산 기초

---
+++ python/calculate.py
@@ -0,0 +1,12 @@
+import io;
+import numpy as np
+
+a, b = input().split();
+print(int(a)+int(b));
+
+a, b = input().split();
+print(int(a)-int(b));
+
+a, b = input().split();
+print(int(a)/int(b));
+
