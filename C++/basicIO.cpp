08b6df7e53dc80402d295d288ef2d5ecb775aa90
유승훈 <juniper0917@gmail.com>
2022-09-09 10:22:25 +0000

    basicIO.cpp

---
+++ C++/basicIO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void)
+{
+	int x,y;
+	cin >> x >> y;
+	cout << "their sum is " << x+y << endl;
+    cout << "Hello, World!";
+	return EXIT_SUCCESS;
+}
